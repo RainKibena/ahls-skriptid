d79b8ffdc6b3247aa58d7f8dfa58e9d1a22757a4
RainKibena <74591178+RainKibena@users.noreply.github.com>
2023-01-16 08:41:41 +0200

    php install skript

--- php.sh
+++ php.sh
@@ -1,10 +1,13 @@
 PHP=$(dpkg-query -W -f='${Status}' php 2>/dev/null | grep -c 'ok installed')
-
-if [ $PHP -eq 0 ]; then
+#PHP installimise skript
+#Kontrollime kas PHP on juba instaleeritud
+if [ $PHP -eq 0 ]; then'
+	#Kui ei ole olemas annab teada ja paigaldab
 	echo "Paigaldame php ja vajalikud lisad"
 	apt install php libapache2-mod-php php-mysql -y
 	echo "php on paigaldatud"
 elif [ $PHP -eq 1 ]; then
+	#Kui olemas annab teada ja ei tee midagi
 	echo "php on juba paigaldatud"
 	which php
 fi
